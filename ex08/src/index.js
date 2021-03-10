20d448f8547e5fb2f15a69eb632dfd0d5b28c8e8
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-11 00:43:28 +0100

    Sum

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,36 @@
-function main() {
+function main(numOne,numTwo) {
     //Only change code below this line
-    numOne = 7;
-    numTwo = 6;
-    sum = numOne + numTwo;
-
-    //Only change code above this line
+    var numOne = 15;
+    var numTwo = 8;
+    var sum = numOne + numTwo;
     return sum;
 
 }
 
-console.log(main(7,6));
+console.log(main(15,8));
 module.exports = main;
+
+function main(num1,num2) {
+    //Only change code below this line
+    var numOne = 5;
+    var numTwo = 28;
+    var sum = num1 + num2;
+    return sum;
+
+}
+
+console.log(main(5,28));
+module.exports = main;
+
+function main(num3,num4) {
+    //Only change code below this line
+    var numOne = 150;
+    var numTwo = 425;
+    var sum = num3 + num4;
+    return sum;
+
+}
+
+console.log(main(150,425));
+module.exports = main;
+
