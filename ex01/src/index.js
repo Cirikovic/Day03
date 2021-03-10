a3c57978827367332c2cba8471a87dbb50400959
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-10 21:09:10 +0100

    Hello World!

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,11 +1,2 @@
-// Setup example
-
-function main() {
-    //Only change code below this line
-
-    //Only change code above this line
-}
-
-console.log(main());
-module.exports = main;
+console.log ("Hello World!");
 
