f7ec8f62c3732184b396cec216bca24b02c0910b
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-11 00:55:32 +0100

    Difference

---
+++ ex09/src/index.js
@@ -0,0 +1,36 @@
+function main(numOne,numTwo) {
+    //Only change code below this line
+    var numOne = 15;
+    var numTwo = 8;
+    var difference = numOne - numTwo;
+    return difference;
+
+}
+
+console.log(main(15,8));
+module.exports = main;
+
+function main(num1,num2) {
+    //Only change code below this line
+    var numOne = 25;
+    var numTwo = 8;
+    var difference = num1 - num2;
+    return difference;
+
+}
+
+console.log(main(25,8));
+module.exports = main;
+
+function main(num3,num4) {
+    //Only change code below this line
+    var numOne = 425;
+    var numTwo = 120;
+    var difference = num3 - num4;
+    return difference;
+
+}
+
+console.log(main(425,120));
+module.exports = main;
+
