5b57a5c7cfc35bee65fcc33a4c98754ebfc8a9d4
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-10 23:28:33 +0100

    numOne + numTwo = sum

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this line
+    numOne = 7;
+    numTwo = 6;
+    sum = numOne + numTwo;
+
+    //Only change code above this line
+    return sum;
+
+}
+
+console.log(main(7,6));
+module.exports = main;
